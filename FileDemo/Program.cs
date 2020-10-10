ba7e670152e2d72d4ba040563089b74dfac3537e
Welbeden <43912308+Welbeden@users.noreply.github.com>
2020-10-10 17:16:55 +0300

    Add files via upload

---
+++ FileDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamWriter writer = File.CreateText(@"c:\newfile.txt"); 
+            writer.WriteLine("First string");
+            writer.WriteLine("Second string"); 
+            writer.Close();
+            StreamReader reader = File.OpenText(@"c:\newfile.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+            Console.ReadKey();
+        }
+    }
+}
